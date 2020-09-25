dbf6e53964bfcd5342427a86367965760504957a
Coby Forrester <cobyforrester@Cobys-MacBook-Pro.local>
2020-09-25 10:04:13 -0700

    fixed francais floating to middle

--- src/base/linkTo.js
+++ src/base/linkTo.js
@@ -18,14 +18,14 @@ export const LinkTo = () => {
     <>
       <div className="container-fluid">
         <div className="row no-gutters container-3 p-2">
-          <div class="col-1 col-xs-2">
+          <div class="col col-xs-1">
             <Link to={link} style={{ textDecoration: "none" }}>
               <div class="btn-cust btn-three text-center">
                 <span>{message}</span>
               </div>
             </Link>
           </div>
-          <div class="col col-lg-8"></div>
+          <div class="col col-lg-9"></div>
           <div className="col col-lg-2 text-right pt-2">
             <button class="button5" onClick={() => alert("Coming soon!")}>
               Français
